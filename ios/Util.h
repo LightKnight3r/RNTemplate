eb10f1e6a3c3863c64db6549ed3e37ed4b88651a
Ngô Thái Sơn <sonnt612@gmail.com>
2021-01-12 21:08:56 +0700

    clean ios

--- ios/Util.h
+++
@@ -1,7 +0,0 @@
-
-
-#import <Foundation/Foundation.h>
-#import "React/RCTBridgeModule.h"
-
-@interface Util : NSObject<RCTBridgeModule>
-@end
--- ios/sanship.swift
+++
@@ -1,9 +0,0 @@
-//
-//  sanship.swift
-//  sanship
-//
-//  Created by Thái Sơn on 1/31/20.
-//  Copyright © 2020 Facebook. All rights reserved.
-//
-
-import Foundation
